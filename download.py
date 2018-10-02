b9de9723d478fd9ca2c83fa86fb2943bc74e7448
NortH <NortH21@yandex.ru>
2018-10-02 21:54:08 +0300

    loggin level

--- download.py
+++ download.py
@@ -4,7 +4,7 @@
 import logging
 import requests
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 CSV = ['Kody_ABC-4kh.csv', 'Kody_DEF-9kh.csv']
 URL = 'https://www.rossvyaz.ru/opendata/7710549038-Rosnumbase/'
@@ -14,18 +14,18 @@ HEADERS = {'user-agent': 'dwn-csv/0.0.1'}
 def get_csv():
     try:
         for i in range(len(CSV)):
-            logging.debug(f'    Print url =  {URL + CSV[i]}')
+            logging.debug(f'Print url =  {URL + CSV[i]}')
             data = requests.get(URL + CSV[i], HEADERS)
             write_file(id=CSV[i], csv=data)
     except:
-        logging.error(f'    Cannot download csv')
+        logging.error(f'Cannot download csv')
 
 def write_file(id, csv):
     try:
         with open(id, "wb") as f:
             f.write(csv.content)
     except:
-        logging.error(f'    Cannot write csv')
+        logging.error(f'Cannot write csv')
 
 def main():
     get_csv()
